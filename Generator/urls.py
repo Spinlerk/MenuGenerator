9583ee6a4e938b0c3531d6f3a84e0b7a613be4bc
Spinlerk <Rostislav.kominek@gmail.com>
2024-05-14 17:34:22 +0200

    dashboard

--- Generator/urls.py
+++ Generator/urls.py
@@ -1,12 +1,15 @@
-from django.contrib import admin
-from django.urls import path, include
-
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import path, include, reverse_lazy
 from . import views
+
 
 app_name = 'Generator'
 
+PasswordChangeView.success_url = reverse_lazy("Generator:password_change_done")
+
 urlpatterns = [
     path("", include('django.contrib.auth.urls')),
-    path("", views.index, name='index'),
+    path("", views.index, name='dashboard'),
+    path("register", views.register, name='register'),
 
 ]
--- Generator/views.py
+++ Generator/views.py
@@ -1,5 +1,26 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .forms import UserRegistrationForm
+
 
 # Create your views here.
+
+@login_required
 def index(request):
     return render(request, 'dashboard.html')
+
+
+def register(request):
+    form = UserRegistrationForm()
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+
+    return render(request, "account/register.html", {'form': form})
+
