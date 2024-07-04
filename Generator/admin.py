59c999948eeffde1c76642ed4de272c0ce509e8e
Spinlerk <Rostislav.kominek@gmail.com>
2024-07-04 18:54:21 +0200

    forms

--- Generator/admin.py
+++ Generator/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import (
     CentralMainCourse,
+    UserMainCourse,
     CentralSoup,
+    UserSoup,
     CentralSalad,
+    UserSalad,
     CentralSideDishes,
+    UserSideDishes,
 )
 from .models import Profile
 
