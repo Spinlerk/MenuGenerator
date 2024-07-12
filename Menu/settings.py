62db999622ef51bb9f7a060562710f8c571690a3
Spinlerk <Rostislav.kominek@gmail.com>
2024-07-12 15:04:16 +0200

    forms

--- Menu/settings.py
+++ Menu/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-^r7kqh%%_7wpcf=vj&&$+ni)nss0qh)t^$bgl^04&t@772y&b6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -129,9 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
